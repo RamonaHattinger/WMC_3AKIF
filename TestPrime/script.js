1577ae01d7174a242fb9171b5459d4013e5bd9db
RamonaHattinger <hat240739@spengergasse.at>
2025-02-20 20:22:25 +0100

    CheckPrime

---
+++ TestPrime/script.js
@@ -0,0 +1,34 @@
+
+
+function isPrime(number) {
+    if (number < 2) return false;
+    
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false;
+    }
+
+    return true;
+    
+}
+
+function checkPrime() {
+    let number = document.getElementById('number').value;
+    let resultElement = document.getElementById('result');
+    
+        if (number === "" || isNaN(number)) {
+        resultElement.textContent = "What the fuck, you're  not even a number!";
+        resultElement.className = "no-prime";
+        return;
+        }
+        
+        number = parseInt(number);
+
+        if (isPrime(number)) {
+            resultElement.textContent = "Yeah baby, you're prime!";
+            resultElement.className = "prime";
+        } 
+        else {
+            resultElement.textContent = "Meh, ...";
+            resultElement.className = "no-prime";
+        }
+}
